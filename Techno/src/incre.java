c65d2c8b5d360931a6e8a5a63f721e4c5dc0cdfd
SUSHMA <SUSHMA@192.168.56.1>
2019-11-15 13:40:26 +0530

    new

---
+++ Techno/src/incre.java
@@ -0,0 +1,15 @@
+
+public class incre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		System.out.println(a);
+		System.out.println(++a);
+		System.out.println(a++);
+		System.out.println(a);
+				
+
+	}
+
+}
