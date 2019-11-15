406bd22585a8a719fe86365057aad99cc25f7a8f
SUSHMA <SUSHMA@192.168.56.1>
2019-11-15 12:06:31 +0530

    to kavitha to kiran

---
+++ Techno/src/Cline.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Cline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner n = new Scanner(System.in);
+		
+		int N=n.nextInt();
+		int a[] = new int[N];
+		
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+			
+
+	}
+
+}
